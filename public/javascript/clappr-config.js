4e9e9e47fd5c780af00e0fab81ea4f9951b3be68
João Paulo <vieira.joaopaulo.91@gmail.com>
2020-10-31 16:01:58 -0300

    refactor(public): add nextVideos config example

--- public/javascript/clappr-config.js
+++ public/javascript/clappr-config.js
@@ -4,6 +4,9 @@ const player = new Clappr.Player({
   source: 'http://clappr.io/highline.mp4',
   poster: 'http://clappr.io/poster.png',
   playback: { controls: true },
+  queue: {
+    nextVideos: ['https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_1920_18MG.mp4'],
+  },
   plugins: [window.QueuePlugin],
 })
 
